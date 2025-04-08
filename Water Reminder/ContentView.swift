69f595bd63c8b7224212e570df8da6bacc7ba475
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-04-08 21:10:55 +0530

    initial app setup

---
+++ Water Reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Reminder
+//
+//  Created by Kiruthika Jeyashankar on 08/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
